7499bf46e9d4ff4d1eece9b40bb0c1ff3ad029fc
Bartłomiej Nowak <bartlomiej.nowak96@gmail.com>
2025-08-08 10:38:22 +0200

    feat: implement responsive canvas that resizes with its container

--- src/components/memory-game/MemoryCanvas.vue
+++ src/components/memory-game/MemoryCanvas.vue
@@ -1,3 +1,16 @@
 <template>
-  <div></div>
+  <div ref="container" class="relative h-full w-full">
+    <div class="absolute top-0 right-0 bottom-0 left-0 flex items-center justify-center">
+      <canvas ref="canvasEl" :width="width" :height="height" class="bg-gray-800" />
+    </div>
+  </div>
 </template>
+<script setup lang="ts">
+import { useTemplateRef, ref } from 'vue'
+import { useElementSize } from '@vueuse/core'
+
+const container = useTemplateRef('container')
+const { width, height } = useElementSize(container)
+
+const canvasEl = ref<HTMLCanvasElement | null>(null)
+</script>
