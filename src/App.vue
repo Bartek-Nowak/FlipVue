7a317580078cf9de6c6510062bb06f2d6c03ab5d
Bartłomiej Nowak <bartlomiej.nowak96@gmail.com>
2025-08-08 09:58:53 +0200

    feat: add memory-game folder with MemoryGame and MemoryCanvas components
    import MemoryGame into app and MemoryCanvas into MemoryGame with initial empty template

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
-<script setup lang="ts">
-</script>
-
 <template>
-  <div></div>
+  <div>
+    <MemoryGame />
+  </div>
 </template>
+<script setup lang="ts">
+import MemoryGame from './components/memory-game/MemoryGame.vue';
+</script>
---
+++ src/components/memory-game/MemoryGame.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <MemoryCanvas />
+  </div>
+</template>
+<script setup lang="ts">
+import MemoryCanvas from './MemoryCanvas.vue';
+</script>
