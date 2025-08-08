54bfca4db3158c36d3a8eed52428e021bac391aa
Bartłomiej Nowak <bartlomiej.nowak96@gmail.com>
2025-08-08 11:27:33 +0200

    feat: add abstract Shape class as base for canvas elements

---
+++ src/memory-game/canvas/shapes/Shape.ts
@@ -0,0 +1,10 @@
+export abstract class Shape {
+  x: number
+  y: number
+  constructor(x: number, y: number) {
+    this.x = x
+    this.y = y
+  }
+
+  abstract draw(ctx: CanvasRenderingContext2D): void
+}
