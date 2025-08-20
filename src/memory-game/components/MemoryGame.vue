10cb8685b79778fdde198df419c57661050bf47a
Bartłomiej Nowak <bartlomiej.nowak96@gmail.com>
2025-08-20 09:54:18 +0200

    style(MemoryGame): adjust layout to maintain aspect ratio for game canvas

--- src/memory-game/components/MemoryGame.vue
+++ src/memory-game/components/MemoryGame.vue
@@ -35,7 +35,7 @@
     </div>
 
     <div
-      class="relative flex w-full flex-1 items-center justify-center rounded-lg bg-gray-700 p-4 shadow-inner"
+      class="relative flex aspect-square w-full items-center justify-center rounded-lg bg-gray-700 p-4 shadow-inner"
     >
       <MemoryCanvas
         :seed="gameStore.seed"
