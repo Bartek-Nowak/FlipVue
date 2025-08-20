530505db2bf77cf347bfd4193939b6de9448c202
Bartłomiej Nowak <bartlomiej.nowak96@gmail.com>
2025-08-20 12:08:01 +0200

    style(Tile): change fallback color from red to gray for improved visual consistency

--- src/memory-game/components/Tile.vue
+++ src/memory-game/components/Tile.vue
@@ -146,7 +146,7 @@ const handlePointerLeave = () => {
     <TresMesh :position="[0, 0, -size[2] / 2 - 0.05]" :rotation="[0, Math.PI, 0]">
       <TresPlaneGeometry :args="[size[0] * 0.9, size[1] * 0.9]" />
       <TresMeshStandardMaterial v-if="planeMaterial" :map="planeMaterial.map" :transparent="true" />
-      <TresMeshStandardMaterial v-else color="red" />
+      <TresMeshStandardMaterial v-else color="gray" />
     </TresMesh>
   </TresMesh>
 </template>
